5272994209652925ccad366057a0d1471a3ce90f
vladstojna <vlad.leic@gmail.com>
2023-01-26 18:59:00 +0000

    add rofi vm management scripts

---
+++ launch-vm-jack-patchbay.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+echoerr() {
+    printf "%s\n" "$*" >&2
+}
+
+print_usage() {
+    echoerr "Usage: $0 <connect|disconnect> <session URI> <domain>"
+}
+
+if [ $# -ne 3 ]; then
+    print_usage
+    exit 1
+fi
+if [ "$1" != 'connect' ] && [ "$1" != 'disconnect' ]; then
+    print_usage
+    exit 1
+fi
+
+jack_cmd="jack_$1"
+session_uri="$2"
+domain="$3"
+
+ardour_session="${ardour_session:-$(ardour-nsm-session-name)}"
+
+output_client="out-$(virsh --connect "$session_uri" dumpxml "$domain" |
+    xmllint --xpath 'string(/domain/devices/audio[@type="jack"]/output/@clientName)' -)"
+
+input_client="in-$(virsh --connect "$session_uri" dumpxml "$domain" |
+    xmllint --xpath 'string(/domain/devices/audio[@type="jack"]/input/@clientName)' -)"
+
+echo "$jack_cmd \"$output_client:output 0\" \"$ardour_session:game/audio_in 1\""
+"$jack_cmd" "$output_client:output 0" "$ardour_session:game/audio_in 1"
+echo "$jack_cmd \"$output_client:output 1\" \"$ardour_session:game/audio_in 2\""
+"$jack_cmd" "$output_client:output 1" "$ardour_session:game/audio_in 2"
+echo "$jack_cmd \"$input_client:input 0\" \"$ardour_session:mic/audio_out 1\""
+"$jack_cmd" "$input_client:input 0" "$ardour_session:mic/audio_out 1"
+echo "$jack_cmd \"$input_client:input 1\" \"$ardour_session:mic/audio_out 2\""
+"$jack_cmd" "$input_client:input 1" "$ardour_session:mic/audio_out 2"
---
+++ rofi-manage-vms.sh
@@ -0,0 +1,201 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+echoerr() {
+    printf "%s\n" "$*" >&2
+}
+
+write_message() {
+    local text="<span font_size=\"medium\">$1</span>"
+    echo -n "$text"
+}
+
+get_spice_port() {
+    local xpath_string='string(/domain/devices/graphics[@type="spice"]/@port)'
+    local port
+    port="$(virsh --connect "$1" dumpxml "$2" | xmllint --xpath "$xpath_string" -)"
+    if [ "$port" -ne -1 ]; then
+        echo "$port"
+    fi
+    return $?
+}
+
+launch_vm() {
+    local file="/tmp/$USER-passthrough-vm-env-$1.conf"
+    echo "PASSTHROUGH_VM_DOMAIN=$1" >"$file"
+    echo "PASSTHROUGH_VM_PORT=$2" >>"$file"
+    systemctl --user start "launch-vm-looking-glass@$1.service"
+    return $?
+}
+
+format_entry() {
+    local separator
+    separator="$(head -c 8 </dev/zero | tr '\0' ' ')"
+    printf "%s$separator" "id: $1" "name: $2" "state: $3"
+}
+
+readonly SESSION_URI='qemu:///system'
+declare -rA ACTIONS=(
+    [start]="Start"
+    [suspend]="Suspend"
+    [shutdown]="Shut down"
+    [resume]="Resume"
+    [looking_glass]="Launch Looking Glass"
+)
+readonly ACTIONS_AFTER_START=(looking_glass suspend shutdown)
+readonly ACTIONS_FALLBACK=(start suspend shutdown resume looking_glass)
+
+declare -A messages
+declare -A virtual_machines
+
+populate_entries() {
+    local id name state
+    while read -r id name state; do
+        messages[$name]=$(write_message "$(format_entry "$id" "$name" "$state")")
+        virtual_machines+=(["$name"]="$state")
+    done < <(virsh --connect "$SESSION_URI" list --all | tail -n +3 | sed '/^[[:space:]]*$/d')
+}
+
+actions_from_state() {
+    local state="$1"
+    case "$state" in
+    "running")
+        echo "${ACTIONS_AFTER_START[@]}"
+        ;;
+    "paused")
+        echo resume
+        ;;
+    "shut off")
+        echo start
+        ;;
+    *)
+        echo "${ACTIONS_FALLBACK[@]}"
+        ;;
+    esac
+}
+
+actions_from_chosen_action() {
+    local action="$1"
+    case "$action" in
+    "suspend" | "shutdown" | "looking_glass")
+        echoerr "actions_from_chosen_action() no more future actions"
+        ;;
+    "resume" | "start")
+        echo "${ACTIONS_AFTER_START[@]}"
+        ;;
+    *)
+        echoerr "actions_from_chosen_action() error"
+        exit 1
+        ;;
+    esac
+}
+
+invalid_selection() {
+    echoerr "Invalid selection: '$1'"
+}
+
+first_menu() {
+    local name
+    echo -e "\0prompt\x1fChoose virtual machine"
+    for name in "${!virtual_machines[@]}"; do
+        echo "${messages[$name]}"
+    done
+}
+
+action_prompts() {
+    local action
+    local name="$1"
+    shift 1
+    echo -e "\0prompt\x1fChoose action for $name"
+    for action in "$@"; do
+        write_message "${ACTIONS[$action]}"
+        # write trailing newline
+        echo
+    done
+}
+
+rofi_data_create_header() {
+    local IFS='|'
+    echo -e "\0data\x1f$*"
+}
+
+second_menu() {
+    local name
+    local selection="$1"
+    echoerr "second_menu() Selection: $selection"
+    for name in "${!virtual_machines[@]}"; do
+        if [ "$selection" = "${messages[$name]}" ]; then
+            local initial_actions
+            initial_actions="$(actions_from_state "${virtual_machines[$name]}")"
+            action_prompts "$name" $initial_actions
+            echoerr "second_menu() success"
+            # set name and actions as data for next execution of script
+            rofi_data_create_header "$name" "$initial_actions"
+            return 0
+        fi
+    done
+    echoerr "second_menu() match failure"
+    return 1
+}
+
+next_menus() {
+    local selection="$1"
+    local name="$2"
+    local actions="$3"
+    echoerr "next_menus() Selection: $selection"
+    local action
+    for action in $actions; do
+        if [ "$selection" = "$(write_message "${ACTIONS[$action]}")" ]; then
+            case "$action" in
+            "start" | "suspend" | "shutdown" | "resume")
+                virsh --connect "$SESSION_URI" "$action" "$name" |
+                    tee >&2 >(xargs -r -I{} notify-send "Virtual Machine: $name" {})
+                echo "$action"
+                ;;
+            "looking_glass")
+                launch_vm "$name" "$(get_spice_port "$SESSION_URI" "$name")" |
+                    tee >&2 >(xargs -r -I{} notify-send "Virtual Machine: $name" {})
+                echo "$action"
+                ;;
+            *)
+                echoerr "next_menus() selection error"
+                return 1
+                ;;
+            esac
+            return 0
+        fi
+    done
+    return 1
+}
+
+main() {
+    echo -e "\0no-custom\x1ftrue"
+    echo -e "\0markup-rows\x1ftrue"
+    populate_entries
+    if [ $# -eq 1 ]; then
+        if ! second_menu "$1"; then
+            local name actions chosen next_actions
+            echoerr "main() ROFI_DATA: $ROFI_DATA"
+            IFS='|' read -r name actions <<<"$ROFI_DATA"
+            chosen=$(next_menus "$1" "$name" "$actions")
+            if [ $? -ne 0 ]; then
+                invalid_selection "$1"
+                exit 1
+            fi
+            next_actions="$(actions_from_chosen_action "$chosen")"
+            echoerr "main() chosen action: $chosen"
+            echoerr "main() next actions: $next_actions"
+            action_prompts "$name" $next_actions
+            rofi_data_create_header "$name" "$next_actions"
+        fi
+    elif [ $# -eq 0 ]; then
+        first_menu
+    else
+        echo "Invalid number of arguments ($# arguments passed, expected 1)" >&2
+        exit 1
+    fi
+}
+
+main "$@"
