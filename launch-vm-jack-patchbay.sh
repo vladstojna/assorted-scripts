48dde616f4e7b360d928e5a9b54cb21a432befc5
vladstojna <vlad.leic@gmail.com>
2023-01-29 21:31:37 +0000

    remove dependency of ardour-nsm-session-name being in PATH

--- launch-vm-jack-patchbay.sh
+++ launch-vm-jack-patchbay.sh
@@ -8,6 +8,10 @@ print_usage() {
     echoerr "Usage: $0 <connect|disconnect> <session URI> <domain>"
 }
 
+get_session_name() {
+    "$(dirname "$(readlink -e "$0")")/ardour-nsm-session-name.sh"
+}
+
 if [ $# -ne 3 ]; then
     print_usage
     exit 1
@@ -21,7 +25,7 @@ jack_cmd="jack_$1"
 session_uri="$2"
 domain="$3"
 
-ardour_session="${ardour_session:-$(ardour-nsm-session-name)}"
+ardour_session="${ardour_session:-$(get_session_name)}"
 
 output_client="out-$(virsh --connect "$session_uri" dumpxml "$domain" |
     xmllint --xpath 'string(/domain/devices/audio[@type="jack"]/output/@clientName)' -)"
