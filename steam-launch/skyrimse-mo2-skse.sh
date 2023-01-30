e9e53f2711f4e22b7a7c250d7fe76dbbbfa74309
vladstojna <vlad.leic@gmail.com>
2023-01-30 04:40:02 +0000

    add steam-launch scripts

---
+++ steam-launch/skyrimse-mo2-skse.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cat <<EOF
+$HOME/.local/share/games/mod-organizer2-skyrimspecialedition/ModOrganizer.exe \
+moshortcut://:SKSE
+EOF
---
+++ steam-launch/skyrimse-wildlander-launcher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat <<EOF
+$HOME/.local/share/Steam/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/\
+AppData/Local/Programs/wildlander-launcher/wildlander-launcher.exe
+EOF
