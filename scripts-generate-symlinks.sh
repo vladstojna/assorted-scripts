f7e61225b7752489cc581a2fd0e628ed4c3235f4
vladstojna <vlad.leic@gmail.com>
2023-01-31 06:02:19 +0000

    scripts-generate-symlinks.sh: extend to work with any target directory

--- scripts-generate-symlinks.sh
+++ scripts-generate-symlinks.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-mkdir -p "$(dirname "$0")/bin" || exit 1
-cd "$(dirname "$0")/bin" || exit 1
+set -e
+set -u
+
+print_usage() {
+    echo "Usage: $0 <target-directory>" >&2
+}
+
+if [ $# -ne 1 ]; then
+    print_usage
+    exit 1
+fi
+
+script_dir="$(realpath -e "$(dirname "$0")")"
+target_dir="$(realpath -e "$1")"
+
+cd "$target_dir"
 
 declare -A existing_links
 while read -r link; do
@@ -18,7 +32,8 @@ while read -r file; do
         ln -sfnv "$target" "$link_name"
     fi
 done < <(
-    find .. -maxdepth 1 -type f -executable |
+    find "$(realpath -e --relative-to="$target_dir" "$script_dir")" \
+        -maxdepth 1 -type f -executable |
         grep -v "$(basename "$0")" |
         sort
 )
