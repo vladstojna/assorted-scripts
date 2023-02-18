13ed085bf4948e929ee6803b7c55567186f15e9e
vladstojna <vlad.leic@gmail.com>
2023-02-18 22:56:01 +0000

    use ROFI_INFO for matching instead of selection text

--- rofi-scratchpad.sh
+++ rofi-scratchpad.sh
@@ -11,20 +11,20 @@ write_message() {
   echo -n "$text"
 }
 
-declare -a hidden_windows
+declare -a scratchpad_windows
 readonly tmp_file_prefix="/tmp/scratchpad-icon"
 
 populate_candidates() {
   local window
   while read -r window; do
-    hidden_windows+=("$window")
+    scratchpad_windows+=("$window")
   done < <(bspwm-scratchpad --list)
 }
 
 get_padding() {
   local window
   local padding=0
-  for window in "${hidden_windows[@]}"; do
+  for window in "${scratchpad_windows[@]}"; do
     class="$(xdotool getwindowclassname "$window")"
     if [ -n "$class" ] && [ "${#class}" -gt "$padding" ]; then
       padding="${#class}"
@@ -45,26 +45,26 @@ get_entry_text() {
 }
 
 menu() {
-  local text padding
-  padding="$(get_padding)"
   local valid_idx=0
-  for window in "${hidden_windows[@]}"; do
+  local padding
+  padding="$(get_padding)"
+  for window in "${scratchpad_windows[@]}"; do
+    local text
     text="$(get_entry_text "$window" "$padding")"
     if [ -n "$text" ]; then
       local icon_path="${tmp_file_prefix}${valid_idx}.png"
       extract-window-icon "$window" >"$icon_path"
-      echo -en "$(write_message "$text")\0icon\x1f${icon_path}\n"
+      echo -en "$(write_message "$text")\0icon\x1f${icon_path}\x1finfo\x1f${window}\n"
       valid_idx=$((valid_idx + 1))
     fi
   done
 }
 
 execute_selection() {
-  local selection="$1"
-  local window padding
-  padding="$(get_padding)"
-  for window in "${hidden_windows[@]}"; do
-    if [ "$(write_message "$(get_entry_text "$window" "$padding")")" = "$selection" ]; then
+  local window
+  for window in "${scratchpad_windows[@]}"; do
+    # compare the current window with the info passed with selected row
+    if [ "$window" = "$ROFI_INFO" ]; then
       bspwm-scratchpad --remove "$window"
     fi
   done
