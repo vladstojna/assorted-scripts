d09a799764717c74ded151c86398040311553ad5
Vlad Stojna <vlad.leic@gmail.com>
2023-04-22 01:57:16 +0100

    Scratchpad (#1)
    * add first version of bspwm & rofi scratchpad
    
    * rewrite scratchpad to use new desktop instead of hiding
    
    * use ROFI_INFO for matching instead of selection text
    
    * add missing --help in usage text
    
    * add command to print desktop name
    
    * add fallback without icon if extract fails
    
    - rudimentary icon cache: no recreation of icon file if file exists

---
+++ bspwm-scratchpad.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+set -eu
+
+print_usage() {
+  cat <<EOF
+  Usage:
+      $0 <option>
+  <option>:
+      --send [window]
+          Put [window] into scratchpad or focused window if [window] is not provided
+      --remove <window>
+          Remove <window> from scratchpad
+      --list
+          List windows present in scratchpad
+      --name
+          Print scratchpad desktop name
+      --help
+          Print this menu and exit
+EOF
+}
+
+scratchpad_name() {
+  echo -en '\u2026'
+}
+
+add_scratchpad_desktop() {
+  local name
+  name="$(scratchpad_name)"
+  if ! bspc query -D --desktop "$name" >/dev/null 2>&1; then
+    bspc monitor --add-desktops "$name"
+    bspc desktop "$name" --layout monocle
+  fi
+  echo "$name"
+}
+
+remove_scratchpad_desktop() {
+  local name
+  name="$(scratchpad_name)"
+  if bspc query -D --desktop "$name" 2>/dev/null && ! bspc query -N -d "$name"; then
+    bspc desktop "$name" --remove
+  fi
+  echo "$name"
+}
+
+currently_focused_window() {
+  bspc query -N -n '.focused'
+}
+
+send_to_scratchpad() {
+  local window
+  local desktop
+  window="${1:-$(currently_focused_window)}"
+  desktop="$(add_scratchpad_desktop)"
+  bspc node "$window" --to-desktop "$desktop"
+}
+
+remove_from_scratchpad() {
+  local window="$1"
+  bspc node "$window" --to-desktop focused --focus
+  remove_scratchpad_desktop >/dev/null
+}
+
+list_scratchpad_windows() {
+  bspc query -N -d "$(scratchpad_name)"
+}
+
+main() {
+  if [ $# -lt 1 ]; then
+    print_usage
+    return 1
+  fi
+
+  case "$1" in
+  "--send")
+    send_to_scratchpad "${2:-}"
+    ;;
+  "--remove")
+    if [ $# -lt 2 ]; then
+      print_usage
+      return 1
+    fi
+    remove_from_scratchpad "$2"
+    ;;
+  "--list")
+    list_scratchpad_windows
+    ;;
+  "--name")
+    scratchpad_name
+    ;;
+  "--help")
+    print_usage
+    ;;
+  *)
+    print_usage
+    return 1
+    ;;
+  esac
+}
+
+main "$@"
---
+++ rofi-scratchpad.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echoerr() {
+  printf "%s\n" "$*" >&2
+}
+
+write_message() {
+  local text="<span font_size=\"medium\">$1</span>"
+  echo -n "$text"
+}
+
+declare -a scratchpad_windows
+readonly tmp_file_prefix="/tmp/scratchpad-icon"
+
+populate_candidates() {
+  local window
+  while read -r window; do
+    scratchpad_windows+=("$window")
+  done < <(bspwm-scratchpad --list)
+}
+
+get_padding() {
+  local window
+  local padding=0
+  for window in "${scratchpad_windows[@]}"; do
+    class="$(xdotool getwindowclassname "$window")"
+    if [ -n "$class" ] && [ "${#class}" -gt "$padding" ]; then
+      padding="${#class}"
+    fi
+  done
+  echo "$padding"
+}
+
+get_entry_text() {
+  local -r window="$1"
+  local -r padding="$2"
+  local class name
+  class="$(xdotool getwindowclassname "$window")"
+  name="$(xdotool getwindowname "$window")"
+  if [ -n "$class" ] && [ -n "$name" ]; then
+    printf "%-${padding}s - %s" "$class" "$name"
+  fi
+}
+
+menu() {
+  local valid_idx=0
+  local padding icon_path icon_meta text
+  padding="$(get_padding)"
+  for window in "${scratchpad_windows[@]}"; do
+    text="$(get_entry_text "$window" "$padding")"
+    if [ -n "$text" ]; then
+      icon_path="${tmp_file_prefix}-$(xdotool getwindowclassname "$window").png"
+      icon_meta="icon\x1f${icon_path}\x1f"
+      if [ ! -f "$icon_path" ] && ! extract-window-icon "$window" >"$icon_path"; then
+        rm "$icon_path"
+        icon_meta=""
+      fi
+      echo -en "$(write_message "$text")\0${icon_meta}info\x1f${window}\n"
+      valid_idx=$((valid_idx + 1))
+    fi
+  done
+}
+
+execute_selection() {
+  local window
+  for window in "${scratchpad_windows[@]}"; do
+    # compare the current window with the info passed with selected row
+    if [ "$window" = "$ROFI_INFO" ]; then
+      bspwm-scratchpad --remove "$window"
+    fi
+  done
+}
+
+main() {
+  echo -e "\0no-custom\x1ftrue"
+  echo -e "\0markup-rows\x1ftrue"
+  echo -e "\0prompt\x1fScratchpad"
+  populate_candidates
+  if [ $# -eq 0 ]; then
+    menu
+  elif [ $# -eq 1 ]; then
+    execute_selection "$1"
+  else
+    echoerr "Invalid number of arguments ($# arguments passed, expected 1)"
+    exit 1
+  fi
+}
+
+main "$@"
